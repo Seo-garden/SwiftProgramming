2fe396ed832b297c07a8012a2cbe6106fb1e51f6
seo-garden <indian4589@naver.com>
2023-09-28 15:51:31 +0900

    0928 commit

--- 0928.playground/Contents.swift
+++ 0928.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 import Foundation
 import SwiftUI
+
+let names: [String] = ["wizplan", "eric", "yagom", "jenny"]
+//13-3
+func backwards(first: String, second: String) -> Bool {
+    print("\(first), \(second) 비교중")
+    return first > second
+}
+let reversed: [String] = names.sorted(by: backwards)
+print(reversed)
+
+
+//13-4
+let reserved: [String] = names.sorted(by: { (first: String, second: String) -> Bool in
+    return first > second
+})
+print(reserved)
